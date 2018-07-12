cb04bd2941cdfd3d1a85e62ae77d3d56734adb53
Stan <varmongar@gmail.com>
2018-07-12 08:50:17 -0600

    Deleted testpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from './Testpage/testpage'
 
 class App extends Component {
   render() {
     return (
       <div>
-          <Test />
+          <h1>Hello</h1>
       </div>
     );
   }
