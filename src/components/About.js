18eaff490cc29f1986bbb4e74e7d6e8e65d054ac
Unknown <mikeperkinsmdp@gmail.com>
2018-07-16 11:37:54 -0600

    Learning changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,13 @@ class About extends Component {
     render () {
         return(
             <div>
+                {/*Routing for Navigation Bar*/}
                 <header>
                     <div className="nav-bar">
                         <a href="/">Home</a>
                         <a href="/content">Content</a>
                         <a className="active" href="/about">About</a>
+                        <a href="/login">Log In / Sign Up</a>
                     </div>
                 </header>
             </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -7,11 +7,13 @@ class Content extends Component {
     render () {
         return(
             <div>
+                {/*Routing for Navigation Bar*/}
                 <header>
                     <div className="nav-bar">
                         <a href="/">Home</a>
                         <a className="active" href="/content">Content</a>
                         <a href="/about">About</a>
+                        <a href="/login">Log In / Sign Up</a>
                     </div>
                 </header>
             </div>
