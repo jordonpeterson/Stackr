2ad3053b18af9c20c3fbd3d2ada63fc4b56f3ac6
Jordon Peterson <jordonpropm@gmail.com>
2018-07-17 16:25:52 -0600

    h1 test

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,12 +10,14 @@ class Content extends Component {
                 {/*Routing for Navigation Bar*/}
                 <header>
                     <div className="nav-bar">
+
                         <a href="/">Home</a>
                         <a className="active" href="/content">Content</a>
                         <a href="/about">About</a>
                         <a href="/login">Log In / Sign Up</a>
                     </div>
                 </header>
+                <h1>Content</h1>
             </div>
         );
     }
